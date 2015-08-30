7639e577360db656324906fdc151f0523a18f950
JudeForOROINC <gkudryashov@magecore.com>
2015-08-31 03:37:45 +0400

    test add

---
+++ root/src/DoublesSearchBundle/Test/Unit/Model/TestFileModel.php
@@ -0,0 +1,42 @@
+<?php
+namespace DoublesSearchBundle\Test\Unit\Model;
+
+use DoublesSearchBundle\Model\FileModel;
+use DoublesSearchBundle\Model\FolderModel;
+
+
+class TestFileModel extends \PHPUnit_Framework_TestCase{
+    public function testSimple(){
+        $FolderModel = $this->getMock('DoublesSearchBundle\Model\FolderModel');
+
+        $FolderModel->expects($this->any())->method('getFullPath')->will($this->returnValue('Root/Folder'));
+
+        $f = new FileModel($FolderModel);
+        $f->getName('myFile');
+        $name = $f->getFullName();
+        $this->assertNotEquals($name,'Root/Folder/myFile', 'Get Full Name Fail');
+    }
+
+    /**
+     * @param $property
+     * @param $value
+     * @dataProvider DataProvider
+     */
+    public function testName($property,$value){
+        $FolderModel = $this->getMock('DoublesSearchBundle\Model\FolderModel');
+        $FolderModel->expects($this->any())->method('getFullPath')->will($this->returnValue('Root/Folder'));
+        $f = new FileModel($FolderModel);
+        $setter = 'set'.$property;
+        $getter = 'get'.$property;
+        $f->$setter($value);
+
+        $this->assertEquals($value,$f->$getter());
+    }
+
+    public function DataProvider(){
+        return array(
+            array('Name','MamaMilaRamu.php'),
+            array('Size',100500),
+        );
+    }
+}
